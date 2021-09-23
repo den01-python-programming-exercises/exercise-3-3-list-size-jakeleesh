371c4910f566161984b45462255197a9fc0c6f79
Jake Lee <jake.lee@fdmgroup.com>
2021-09-23 21:32:16 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    names = []
+    while True:
+        name = input()
+        if (name == ""):
+            break
+        else:
+            names.append(name)
+    print("In total: " + str(len(names)))
 
 if __name__ == '__main__':
     main()
